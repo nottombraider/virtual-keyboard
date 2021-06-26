c66ff8af09935969ff5210e58a4e9dbc609afdba
nottombraider <larysa.yavorska@outlook.com>
2021-06-26 10:36:36 +0200

    feat: add header

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  const titleContent = 'MacOS Keyboard &#63743;';
+  const header = document.createElement('header');
+  const titleH1 = document.createElement('h1');
+
+  titleH1.innerHTML = titleContent;
+  header.appendChild(titleH1);
+
+  return header;
+};
+
+export default Header;
